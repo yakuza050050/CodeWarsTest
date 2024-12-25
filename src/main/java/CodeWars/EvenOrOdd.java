5e8cb69152635e20157d9eee19715c222bd477c2
Aza <aziko050050@gmail.com>
2024-12-25 21:11:19 +0600

    Tesk/Even_Or_Odd

---
+++ src/main/java/CodeWars/EvenOrOdd.java
@@ -0,0 +1,15 @@
+package CodeWars;
+
+public class EvenOrOdd {
+    /*
+    Create a function that takes
+    an integer as an argument and returns "Even"
+    for even numbers or "Odd" for odd numbers.
+*/
+    public String evenOrGold(int num) {
+        if(num == 6) {
+            return "Even";
+        } else return "Odd";
+    }
+
+}
