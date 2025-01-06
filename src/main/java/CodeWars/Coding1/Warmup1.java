51dd43dc2fac81cac142d68cb7c804daa104d180
kai <baialievakaiyrgul@gmail.com>
2025-01-06 22:38:27 +0600

    Codingexample

--- src/main/java/CodeWars/Coding1/Warmup1.java
+++ src/main/java/CodeWars/Coding1/Warmup1.java
@@ -3,5 +3,6 @@ package CodeWars.Coding1;
 public class Warmup1 {
     public static void main(String[] args) {
         System.out.println("Please password");
+        System.out.println("5463484");
     }
 }
